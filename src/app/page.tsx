4c225a33072d8a486a365a25858d2fd320a6bb1c
Ibraheem Alyan <35265448+ibraheemalayan@users.noreply.github.com>
2024-06-23 15:32:11 +0300

    chore: 🎉 begin project

---
+++ src/app/page.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import React from 'react';
+import { Tabs, Flex, Card } from 'antd';
+import type { TabsProps } from 'antd';
+
+import BitRateTab from './bit_rate_tab';
+
+
+const onChange = (key: string) => {
+  console.log(key);
+};
+
+const items: TabsProps['items'] = [
+  {
+    key: '1',
+    label: 'Bit/Rate',
+    children: <BitRateTab />,
+  },
+  {
+    key: '2',
+    label: 'Tab 2',
+    children: 'Content of Tab Pane 2',
+  },
+  {
+    key: '3',
+    label: 'Tab 3',
+    children: 'Content of Tab Pane 3',
+  },
+];
+
+export default function Home() {
+  return (
+    <main className="flex flex-col min-h-screen text-center content-center items-center p-6">
+      <h1 className="text-3xl font-bold text-center mb-8">Wireless Communications Calculator</h1>
+      <Flex justify='center' align='flex-start' vertical>
+
+        <Card title="Select a calculater" bordered={false} style={{ width: 1000 }}>
+          <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+        </Card>
+
+      </Flex>
+      <footer className="text-center text-sm mt-12 text-gray-600">
+        <p>&copy; 2024 Wireless Calculator, Birzeit University, Ibraheem Alyan & Adham Al Masri</p>
+      </footer>
+    </main>
+  );
+}
